f120d084d2c8e972aec10b71f3ac2f883f8a412a
PRRAG <pavlyr.2019@gmail.com>
2021-11-21 01:49:14 +0200

    Update - hide the navigation when the user scrolls down the page

--- js/app.js
+++ js/app.js
@@ -16,18 +16,19 @@
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
 let frag = document.createDocumentFragment();
 let nav = document.getElementById("navbar__list");
 let findMe = document.querySelectorAll('.landing__container');
 let sections = document.querySelectorAll('section');
 let navItem;
+let prevScrollpos = window.pageYOffset;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -41,7 +42,7 @@ const isInViewport = function (elem) {
     let distance = elem.getBoundingClientRect();
     return (
         distance.top >= 0 &&
-        distance.bottom <= (window.innerHeight + distance.height - 200 || document.documentElement.clientHeight + distance.height)
+        distance.top <= 250
     );
 };
 
@@ -69,11 +70,14 @@ function buildNav() {
 // Add class 'active' to section when near top of viewport
 function setActive() {
     window.addEventListener('scroll', function (event) {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar__list").style.top = "0";
+        } else {
+            document.getElementById("navbar__list").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
         for (let i = 0; i < sections.length; i++) {
-            nav.style.display = 'flex';
-            setTimeout(function () {
-                nav.style.display = 'none';
-            }, 10000);
             if (isInViewport(sections[i])) {
                 sections[i].classList.add("active");
                 navItem[i].className = "active_nav";
@@ -99,7 +103,7 @@ function scrollTO() {
 
         listItem[i].addEventListener('click', function () {
             sections[i].scrollIntoView({ behavior: "smooth" });
-            
+
         });
     }
 };
